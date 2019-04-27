1170144740d71dc8f4fc2080a98bab52a41220fa
William Buck <wsbuckiii@gmail.com>
2019-04-26 17:32:11 -0700

    Updates

---
+++ precache-manifest.e02916e24d57b982c57a78df7915d7d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a039e5ceefbb5f1850427c4f28ddb9b",
+    "url": "/drum-kit/index.html"
+  },
+  {
+    "revision": "0ddf8ae41afb86f59e57",
+    "url": "/drum-kit/static/css/main.6e73a989.chunk.css"
+  },
+  {
+    "revision": "720aeb63b182f7262046",
+    "url": "/drum-kit/static/js/2.6fc880a1.chunk.js"
+  },
+  {
+    "revision": "0ddf8ae41afb86f59e57",
+    "url": "/drum-kit/static/js/main.235a5045.chunk.js"
+  },
+  {
+    "revision": "7b6327eefb8c5630e0e5",
+    "url": "/drum-kit/static/js/runtime~main.3ae46f20.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/drum-kit/static/media/logo.5d5d9eef.svg"
+  }
+]);
